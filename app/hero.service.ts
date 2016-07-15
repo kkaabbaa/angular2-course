1a7d8ad18a619ea74791a7219658a4c01cfbc740
kkaabbaa <nasnetstep@gmail.com>
2016-07-15 19:05:54 +0300

    dashboar with routes

---
+++ app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+	getHeroes() {
+		return Promise.resolve(HEROES);
+	}
+
+	getHero(id: number) {
+		return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id))
+	}
+
+	getHeroesSlowly() {
+		return new Promise<Hero[]>(
+				resolve =>
+						setTimeout(()=>resolve(HEROES), 2000)
+		);
+	}
+}
